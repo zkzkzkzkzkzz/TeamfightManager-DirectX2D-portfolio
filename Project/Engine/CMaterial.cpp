6e98cc40e9cda67b05afe236a64c8cc6d264dda3
JinWoo Shin <sto134679@gmail.com>
2024-03-25 18:37:57 +0900

    #1 머테리얼 fclose함수 누락 수정

--- Project/Engine/CMaterial.cpp
+++ Project/Engine/CMaterial.cpp
@@ -135,6 +135,8 @@ int CMaterial::Save(const wstring& _strRelativePath)
 	// 재질이 참조하는 셰이더 정보를 저장
 	SaveAssetRef<CGraphicsShader>(m_pShader, pFile);
 
+	fclose(pFile);
+
 	return 0;
 }
 
@@ -158,6 +160,8 @@ int CMaterial::Load(const wstring& _strFilePath)
 
 	// 재질이 참조하는 셰이더 정보를 로드
 	LoadAssetRef<CGraphicsShader>(m_pShader, pFile);
+
+	fclose(pFile);
 
 	return 0;
 }
